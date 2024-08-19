151e371f83ced4a8a90520bd13f6353098863f61
Jordan Hoey <jordan.hoey@justice.gov.uk>
2024-08-19 15:44:50 +0100

    t

--- scripts/common/common-functions.sh
+++ scripts/common/common-functions.sh
@@ -152,7 +152,7 @@ function is_weekend_in_range() {
 }
 
 # Example usage with DD/MM/YYYY format
-is_weekend_in_range "09/08/2024" "12/08/2024"
+is_weekend_in_range "09-08-2024" "12-08-2024"
 
 function should_skip_start_stop () {
   local env business_area issue
